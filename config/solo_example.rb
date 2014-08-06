36921721baffccdb2fce56e0da44332a68cadc32
Suguru Akiho <suguru.akiho@gmail.com>
2014-08-06 17:08:49 +0900

    Update solo_example.rb

--- config/solo_example.rb
+++ config/solo_example.rb
@@ -1,6 +1,6 @@
 node_name "jobscheduler"
 cookbook_path ["/path/to/jobscheduler-chef-repo/cookbooks", "/path/to/jobscheduler-chef-repo/site-cookbooks"]
-role_path "/path/to/jobscheduler/roles"
+role_path "/path/to/jobscheduler-chef-repo/roles"
 # data_bag_path ""
 # environment_path ""
 # node_path ""
